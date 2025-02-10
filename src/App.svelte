d6dda267063ff437ff178532b50f53db65f6d505
C4r50nC <xichen059@gmail.com>
2025-02-11 07:53:59 +1100

    feat: add products array to pass in component arguments as objects

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script>
   import Product from './Product.svelte';
 
+  let products = [
+    {
+      id: 'p1',
+      title: 'A Book',
+      price: 9.99,
+    },
+  ];
+
   function addToCart(event) {
     console.log(event.detail);
   }
@@ -10,8 +18,8 @@
   }
 </script>
 
-<Product
-  productTitle="A Book"
-  on:add-to-cart={addToCart}
-  on:delete={deleteProduct}
-/>
+{#each products as product}
+  <!-- Pass in arguments (id, title, and price) using spread operator. Parameter names have to match exactly. -->
+  <!-- Parameters that does not exist (id) in the component can be passed in but cannot be used. -->
+  <Product {...product} on:add-to-cart={addToCart} on:delete={deleteProduct} />
+{/each}
